ca1bd0a6963d64a773633bb00498f874b3e60865
Rizbir Khan <ec18115@qmul.ac.uk>
2022-02-14 19:17:52 +0000

    Adding overlayed content script

--- public/background.js
+++ public/background.js
@@ -3,12 +3,11 @@
 chrome.runtime.onMessage.addListener(message => {
   switch (message.action) {
     case "SEND_PASSWORD":
-      console.log(message.payload)
       chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
         const tab = tabs[0]
         const url = new URL(tab.url)
         const hostname = url.hostname
-        chrome.runtime.sendMessage({
+        chrome.tabs.sendMessage(tab.id, {
           action: "SHOW_PASSWORD_NOTIFICATION",
           payload: {
             useCase: hostname,
