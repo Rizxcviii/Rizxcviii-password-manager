2a80e6a2699355830157dabd172e26eb1e73903a
Rizbir Khan <ec18115@qmul.ac.uk>
2022-02-14 19:21:00 +0000

    adding box-shadow to signal overlay

--- public/content-script.js
+++ public/content-script.js
@@ -12,7 +12,7 @@ const handleSubmit = form => {
 const createHTMLNotification = ({ useCase, password }) =>
   `
     <!-- New password detected: MAKE WAYYYYY FOR THIS -->
-    <div style="z-index: 69420 ;width: 300px; position: fixed;top: 16px;right: 16px;border: 1px solid #DEF2F1;border-radius: 8px;padding: 16px;font-family: 'Centra';background-color: azure;" id="add-password-rizxcviii">
+    <div style="box-shadow: 1px 4px 8px 2px rgb(0 0 0 / 20%); z-index: 69420 ;width: 300px; position: fixed;top: 16px;right: 16px;border: 1px solid #DEF2F1;border-radius: 8px;padding: 16px;font-family: 'Centra';background-color: azure;" id="add-password-rizxcviii">
       <h1 style="font-size: 24px; margin: 0; margin-bottom: 16px; font-family: 'Centra';">Rizxcviii - Add Password</h1>
       <p style="font-weight: 100;">Would you like to add a password for <b style="font-weight: 500;">${useCase}</b>?</p>
       <form onsubmit="e => addPassword(e)">
